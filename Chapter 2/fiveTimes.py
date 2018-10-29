54ce5d56996907ced242f2630ff858c359149b0a
Masen Matthews <masenmatthews@gmail.com>
2018-10-28 18:46:27 -0700

    add chapter two notes

---
+++ Chapter 2/fiveTimes.py
@@ -0,0 +1,13 @@
+# for loop
+
+print('My name is')
+for i in range(5):
+    print('Jimmy Five Times (' + str(i) + ')')
+
+# equivalent while loop
+
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i = i + 1
