f0deec4ea2539c36b760e3b59502836611bb91ca
Masen Matthews <masenmatthews@gmail.com>
2018-10-30 19:15:55 -0700

    add introductory chapter 4 notes

---
+++ Chapter 4/magic8Ball2.py
@@ -0,0 +1,15 @@
+import random
+
+messages = ['It is certain',
+    'It is decidedly so',
+    'Yes definitely',
+    'Reply hazy try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful']
+
+print(messages[random.randint(0, len(messages) - 1)])
+
+# see Chapter 3 for original version of this "magic 8 ball" program
